77786f3673a88e133a28f93cc4ea17f65e5b6bb0
puchtaw <admin@pwk1894.pl>
2016-12-06 15:22:26 +0100

    Update runTest.sh

--- runTest.sh
+++ runTest.sh
@@ -2,28 +2,37 @@
 
 # Simple wrapper for running ipcalc test suite
 
-echo "runTest.sh, v 1.0"
+echo "runTest.sh, v 1.0\n"
 
-if [ -d bin ]; then
-    CLSS_PATH=bin/
-else
+# create bin directory if it doesn`t exist
+if [ ! -d bin ]; then
     mkdir bin
-    CLSS_PATH=bin/
 fi
 
-JUNIT_JAR=/usr/local/junit/* # should be changed to match your installation
-TEST_CLSS_PATH=$CLSS_PATH:tst/bin:$JUNIT_JAR
+CLSS_PATH=bin/
+JUNIT_JAR=/usr/local/junit # should be changed to match your installation
+TEST_CLSS_PATH=$CLSS_PATH:tst/bin:$JUNIT_JAR/*
 SOURCE_FILE=src/IpCalc.java
 TEST_SRC="tst/IpCalcTest.java tst/TestSuite.java tst/TestRunner.java"
 TEST_BIN=ipcalc/TestRunner
 
+# check if JUNIT_JAR exists
+if [ -d $JUNIT_JAR ]; then
+    echo "\tjunit directory: $JUNIT_JAR \n\tOK!"
+else 
+    echo "Change JUNIT_JAR variable to match your installation!"
+    echo "Current junit directory is set to $JUNIT_JAR"
+    exit 1
+fi
 
-echo "\n\tcompiling IpCalc.java"
-javac -d $CLSS_PATH $SOURCE_FILE && \ 
+
+echo "\n\tcompiling IpCalc.java..."
+javac -d $CLSS_PATH $SOURCE_FILE && \
                         echo "\tOK!" 
 
 echo "\n\tcompiling tests..."
 javac   -d tst/bin -cp $TEST_CLSS_PATH $TEST_SRC && \
                                     echo "\tOK!" && \
-                echo "\n\trunning test suite..." && \
+                echo "\n\trunning test suite:\n" 
+
 java -cp $TEST_CLSS_PATH $TEST_BIN
